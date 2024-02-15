d62be34165c258a38fdff3b7d20bb795027eddf2
Riri1809 <issrach41@yahoo.com>
2024-02-15 12:23:58 -0500

    commit

---
+++ src/pages/Account.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Account() {
+  return (
+    <div className='w-full h-screen bg-[#4169E1] text-white flex justify-center items-center'>
+      <h2 className='text-3xl'>ACCOUNT</h2>
+    </div>
+  )
+}
+
+export default Account
